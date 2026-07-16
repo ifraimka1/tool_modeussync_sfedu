9fa8ae947b6c1ba35f0c05cce46b66993c1c73e8
ifraimka.anthill <mr.ifraim@yandex.ru>
2026-07-16 19:41:15 +0300

    monorepo changes

--- classes/courses_consts.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace tool_modeussync;
-
-class courses_consts
-{
-    /** Эти типы элементов не умеем создавать */
-    public static $unsupported_module_types = array("h5pactivity", "scorm", "imscp");
-}
